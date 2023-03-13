bf2552b2f0227a974c4d8115ba8bff84e1c2ab36
TikoMyster <tikomyster20@gmail.com>
2023-03-13 00:24:21 -0700

    updated html, added icons and audio

---
+++ congrats_app/src/compenents/head.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const head = () => {
+  return (
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Congratulations</title>
+
+    <link rel="icon" href="assets/W-Logo_Purple_Hex.png"/>
+    <link rel="stylesheet" href="https://kit.fontawesome.com/9913f8c9a6.css" crossorigin="anonymous"/>
+   
+        <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
+        rel="stylesheet"/>   
+    
+        
+        <link rel="stylesheet" href="style.css" />
+    </head>
+  );
+};
+
+export default head
